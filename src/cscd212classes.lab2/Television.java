21046ba2d541ca4c1821fea9061e423dca7e3aff
Chris Cornell <ccornell1@ewu.edu>
2023-04-07 19:08:20 -0700

    added Television Class and comparators

--- src/cscd212classes.lab2/Television.java
+++ src/cscd212classes.lab2/Television.java
@@ -8,13 +8,14 @@ public class Television implements Comparable<Television> {
     private final boolean smart;
     private final boolean fourK;
 
+
     // Constructor
     public Television(String make, String model, boolean smart, int screenSize, int resolution) {
         if (make == null || make.isEmpty() || model == null || model.isEmpty()) {
-            throw new IllegalArgumentException("make, model, and resolution cannot be null or empty");
+            throw new IllegalArgumentException("Invalid parameter in constructor");
         }
         if (screenSize < 32 || resolution < 720) {
-            throw new IllegalArgumentException("screenSize must be at least 32 inches and resolution must be at least 720p");
+            throw new IllegalArgumentException("Invalid parameter in constructor");
         }
 
         this.make = make;
@@ -22,15 +23,27 @@ public class Television implements Comparable<Television> {
         this.smart = smart;
         this.screenSize = screenSize;
         this.resolution = resolution;
+        if(this.resolution == 2160) {
+            this.fourK = true;
+        }
+        else {
+            this.fourK = false;
+        }
     }
 
     //Constructor
     public Television(String model, boolean smart, int screenSize, int resolution, String make) {
-        this.make = make;
         this.model = model;
         this.smart = smart;
         this.screenSize = screenSize;
         this.resolution = resolution;
+        this.make = make;
+        if(this.resolution == 2160) {
+            this.fourK = true;
+        }
+        else {
+            this.fourK = false;
+        }
 
     }
 
@@ -50,19 +63,27 @@ public class Television implements Comparable<Television> {
     public int getScreenSize() {
         return screenSize;
     }
+
+    // Implementing the toString method
     @Override
     public String toString() {
-        String smartString = "";
-        String resolutionString = "";
-        if (smart && resolution == 2160) {
-            smartString = " smart";
+        if(this.smart && this.fourK){
+
+            return this.make + "-" + this.model + ", " + this.screenSize + " inch " + "smart tv with " + this.resolution + " resolution";
         }
-        if (fourK) {
-            resolutionString = " with 4K resolution";
-        } else {
-            resolutionString = " with " + resolution + " resolution";
+        else if(this.smart){
+
+            return this.make + "-" + this.model + ", " + this.screenSize + " inch " + "smart tv with " + resolution + " resolution";
         }
-        return make + "-" + model + ", " + screenSize + " inch" + smartString + " tv" + resolutionString;
+        else if(this.fourK){
+
+            return this.make + "-" + this.model + ", " + this.screenSize + " inch " + "tv with 4K resolution";
+
+        }
+        else{
+            return this.make + "-" + this.model + ", " + this.screenSize + " inch " + "tv with " + resolution + " resolution";
+        }
+
     }
 
     // equals method.
@@ -76,7 +97,7 @@ public class Television implements Comparable<Television> {
         }
         Television that = (Television) o;
         return this.make.equals(that.make)
-                && this.model.equals(that.model)
+                && this.model == that.model
                 && this.smart == that.smart
                 && this.screenSize == that.screenSize
                 && this.resolution == that.resolution
@@ -84,12 +105,68 @@ public class Television implements Comparable<Television> {
     }
     @Override
     public int hashCode() {
-        return this.make.hashCode() + this.model.hashCode() + this.resolution + Boolean.hashCode(this.smart) + Boolean.hashCode(fourK);
+        return this.make.hashCode() + this.model.hashCode() + this.resolution + Boolean.hashCode(smart) + Boolean.hashCode(fourK);
+    }
+    @Override
+    public int compareTo(Television another) {
+        if (another == null) {
+            throw new IllegalArgumentException("null parameter in the compareTo method");
+        }
+
+        int makeCompare = this.make.compareTo(another.make);
+        if (makeCompare != 0) {
+            return makeCompare;
+        }
+
+        int modelCompare = this.model.compareTo(another.model);
+        if (modelCompare != 0) {
+            return modelCompare;
+        }
+
+        return this.screenSize - another.getScreenSize();
     }
 
-    //CompareTo Method. Compares based on make.
-    @Override
-    public int compareTo(Television another ){
-        return make.compareTo(another.make);
+   /* @Override
+    public int compareTo(Television another) {
+        if (another == null) {
+            throw new IllegalArgumentException("null parameter in the compareTo method");
+        }
+
+        int makeCompare = this.make.compareTo(another.make);
+        if (makeCompare != 0) {
+            return makeCompare;
+        }
+
+        int modelCompare = this.model.compareTo(another.model);
+        if (modelCompare != 0) {
+            return modelCompare;
+        }
+        return Integer.compare(another.screenSize, this.screenSize);
     }
+
+ /* @Override
+  public int compareTo(Television another) {
+      if (another == null) {
+          throw new IllegalArgumentException("null parameter in the compareTo method");
+      }
+
+      int makeCompare = this.make.compareTo(another.make);
+      if (makeCompare != 0) {
+          return makeCompare;
+      }
+
+      int modelCompare = this.model.compareTo(another.model);
+      if (modelCompare != 0) {
+          return modelCompare;
+      }
+
+      if (this.screenSize < another.screenSize) {
+          return -1;
+      } else if (this.screenSize > another.screenSize) {
+          return 1;
+      } else {
+          return 0;
+      }
+  }*/
 }
+
--- src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
+++ src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
@@ -1,2 +1,21 @@
-package cscd212comparators.lab2;public class TelevisionResolutionMakeDescendingComparator {
+package cscd212comparators.lab2;
+import cscd212classes.lab2.Television;
+import java.util.Comparator;
+
+public class TelevisionResolutionMakeDescendingComparator implements Comparator<Television> {
+    public int compare(Television t1, Television t2) {
+        if (t1 == null || t2 == null) {
+            throw new IllegalArgumentException("null parameter in TelevisionResolutionMakeDescendingComparator");
+        }
+        int makeComparison = t2.getMake().compareTo(t1.getMake());
+        if (makeComparison != 0) {
+            return makeComparison;
+        }
+        return t2.getResolution() - t1.getResolution();
+    }
 }
+
+
+
+
+
